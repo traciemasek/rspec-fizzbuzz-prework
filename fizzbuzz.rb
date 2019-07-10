d06fd2fded2d8d75d68ff77955cd78033aca0289
Tracie Masek <traciemasek@gmail.com>
2019-07-10 10:52:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   if num % 3 == 0 
-    return "fizz"
+    return "Fizz"
   elsif num % 5 == 0 
-    return "buzz"
+    return "Buzz"
   elsif num % 3 == 0 && num % 5 == 0
-    return "fizzbuzz"
+    return "FizzBuzz"
   else 
     return nil
   end
